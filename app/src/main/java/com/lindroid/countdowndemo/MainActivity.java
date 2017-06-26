230216a69ade30896daf91489db9b2a51644da98
Lindroid <linyulong2014@163.com>
2017-06-26 16:39:56 +0800

    1、解决了CountdownTimer倒计时不准确的bug； 2、给倒计时读数和单位设置前景色。

--- app/src/main/java/com/lindroid/countdowndemo/MainActivity.java
+++ app/src/main/java/com/lindroid/countdowndemo/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Context context;
     //    private EditText editText;
     private Button btnCaptcha;
+    private SpannableStringBuilder sb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +28,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnCaptcha = (Button) findViewById(R.id.btn_captcha);
         btnCaptcha.setOnClickListener(this);
         countTimer = new CountTimer(10000, 1000);
+        sb = new SpannableStringBuilder();
     }
 
     @Override
     public void onClick(View v) {
-//        if (!isPhoneNumber(editText.getText().toString())) {
-//            Toast.makeText(context, "请输入正确的手机号码", Toast.LENGTH_SHORT).show();
-//            return;
-//        }else {
-//        }
         countTimer.start();
     }
 
@@ -56,13 +56,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         /**
          * 倒计时过程中调用
-         *
          * @param millisUntilFinished
          */
         @Override
         public void onTick(long millisUntilFinished) {
             Log.e("Tag", "millisUntilFinished=" + millisUntilFinished);
-            btnCaptcha.setText(millisUntilFinished / 1000 + "s后重新发送");
+//            Log.e("Tag", "倒计时=" + (millisUntilFinished/1000));
+            //处理后的倒计时数值
+            int time = (int) (Math.round((double) millisUntilFinished / 1000) - 1);
+//            btnCaptcha.setText(String.valueOf(time)+"s后重新发送");
+            //拼接要显示的字符串
+            sb.clear();
+            Log.e("Tag", "字符长度=" + sb.length());
+            sb.append(String.valueOf(time));
+            sb.append("s后重新发送");
+            int index = String.valueOf(sb).indexOf("后");
+            Log.e("Tag", "字符长度2=" + sb.length() + ",index=" + index);
+            //给秒数和单位设置蓝色前景色
+            ForegroundColorSpan colorSpan = new ForegroundColorSpan(ContextCompat.getColor(context, android.R.color.holo_blue_dark));
+            sb.setSpan(colorSpan, 0, index, Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+            btnCaptcha.setText(sb);
             //设置倒计时中的按钮外观
             btnCaptcha.setClickable(false);//倒计时过程中将按钮设置为不可点击
             btnCaptcha.setBackgroundColor(Color.parseColor("#c7c7c7"));
